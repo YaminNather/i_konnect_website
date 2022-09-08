e107cc7be4aaf0778086e077190a24e7428eda96
Yamin Nather <2001s.yn@gmail.com>
2022-09-08 22:16:38 +0530

    Setup dissappearing NavBar

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,10 +1,21 @@
 import classNames from "classnames";
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+import Image from "next/image";
 import styles from "../styles/LandingPage.module.scss";
+import heroImage from "../public/hero.jpeg";
 
 export const HeroSection: FC = (props) => {
+    const [imageOffset, setImageOffset] = useState<number>(0);
+
+    useEffect(
+        () => {
+            window.addEventListener( "scroll", (event) => setImageOffset(window.scrollY / 2) );
+        },
+        []
+    );
+
     return (
-        <section id="hero_section" className={classNames("container", styles.hero_section)}>
+        <section id="hero_section" className={classNames(styles.hero_section)}>
             <div className={styles.text_area}>
                 <div className={styles.text_container}>
                     <h1>Coimbatore&#39;s Most Trusted Laptop Servicing</h1>
@@ -15,7 +26,9 @@ export const HeroSection: FC = (props) => {
                 </div>
             </div>
 
-            <div style={{ backgroundColor: "red" }} className={styles.image} />
+            <div className={styles.image_container}>                
+                <Image src={heroImage} style={{transform: `translate(0px, +${imageOffset}px)`}} className={styles.image} layout="fill" objectFit="cover" />
+            </div>
         </section>
     );
 };
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,28 @@
 import classnames from "classnames";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import Image from "next/image";
 import styles from "../styles/LandingPage.module.scss";
+import companyLogo from "../public/company-logo.png";
 
 export const NavBar: FC = () => {
+    const [isVisible, setIsVisible] = useState<boolean>(true);    
+    
+    useEffect(
+        () => {
+            window.addEventListener(
+                "scroll", 
+                (event) => {
+                    if(isVisible && window.scrollY >= 100) setIsVisible(false);
+                    else if(!isVisible && window.scrollY < 100) setIsVisible(true);
+                }
+            );
+        },
+        []
+    );
+
     return (
-        <div className={classnames("container", styles.navbar)}>
-            <div style={{width: "128px", height: "60px", backgroundColor: "red"}} />
+        <div style={{opacity: (isVisible) ? "1.0" : "0.0"}} className={classnames("container", styles.navbar)}>            
+            <Image src={companyLogo} />
         </div>
     );
 };
