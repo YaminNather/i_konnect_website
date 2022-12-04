1eea4b1aa759f829b69adca13f4242058a489678
YaminNather <2001s.yn@gmail.com>
2022-12-04 12:18:47 +0530

    Changed get a quote form issues parameter name to issues[]

--- components/sales_lead_page/sales_lead_section/get_a_quote_form/get_a_quote_form.tsx
+++ components/sales_lead_page/sales_lead_section/get_a_quote_form/get_a_quote_form.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { CSSProperties, FC } from "react";
+import { CSSProperties, FC, FormEvent, useCallback } from "react";
 import styles from "./get_a_quote_form_styles.module.scss";
 
 export interface GetAQuoteFormProps {
@@ -8,8 +8,31 @@ export interface GetAQuoteFormProps {
 }
 
 export const GetAQuoteForm: FC<GetAQuoteFormProps> = (props) => {
+    const onSubmit = useCallback(
+        (event: FormEvent): void => {
+            // event.preventDefault();
+
+            const formData: FormData = new FormData(event.target as HTMLFormElement);
+            const formValues: { [key: string]: any } = {};
+
+            const keysIterator: IterableIterator<string> = formData.keys();
+            let iteratorResult: IteratorResult<string, any> = keysIterator.next();
+            while(!iteratorResult.done) {
+                const key: string = iteratorResult.value;
+                
+                if(key.includes("[]")) formValues[key] = formData.getAll(key);
+                else formValues[key] = formData.get(key);
+
+                iteratorResult = keysIterator.next();
+            }            
+
+            console.log(`CustomLog: Form Data = ${JSON.stringify(formValues, null, 2)}`);
+        },
+        []
+    );
+
     return (
-        <form style={props.style} className={classNames(styles.get_a_quote_form, props.className)}>
+        <form style={props.style} onSubmit={onSubmit} className={classNames(styles.get_a_quote_form, props.className)}>
             <h2>Get a Free Quotation Now!</h2>
 
             <label htmlFor="name" className={styles.name_label}>Name</label>
@@ -19,43 +42,43 @@ export const GetAQuoteForm: FC<GetAQuoteFormProps> = (props) => {
             <p className={styles.choose_your_issues_heading}>Choose your issues:</p>
 
             <div className={styles.checkbox_with_label_container}>
-                <input type="checkbox" id="issue_display" name="issues" value="Display" />
+                <input type="checkbox" id="issue_display" name="issues[]" value="Display" />
 
                 <label htmlFor="issue_display">Display</label>
             </div>
             
             <div className={styles.checkbox_with_label_container}>
-                <input type="checkbox" id="issue_motherboard" name="issues" value="Motherboard" />
+                <input type="checkbox" id="issue_motherboard" name="issues[]" value="Motherboard" />
 
                 <label htmlFor="issue_motherboard">Motherboard</label>
             </div>
             
             <div className={styles.checkbox_with_label_container}>
-                <input type="checkbox" id="issue_laptop_overall" name="issues" value="Laptop Overall" />
+                <input type="checkbox" id="issue_laptop_overall" name="issues[]" value="Laptop Overall" />
 
                 <label htmlFor="issue_laptop_overall">Laptop Overall</label>
             </div>
             
             <div className={styles.checkbox_with_label_container}>
-                <input type="checkbox" id="CPU" name="issues" value="CPU" />
+                <input type="checkbox" id="CPU" name="issues[]" value="CPU" />
 
                 <label htmlFor="CPU">CPU</label>
             </div>
             
             <div className={styles.checkbox_with_label_container}>
-                <input type="checkbox" id="issue_hard_disk" name="issues" value="Hard Disk" />
+                <input type="checkbox" id="issue_hard_disk" name="issues[]" value="Hard Disk" />
 
                 <label htmlFor="issue_hard_disk">Hard Disk</label>
             </div>
             
             <div className={styles.checkbox_with_label_container}>
-                <input type="checkbox" id="issue_software_packages" name="issues" value="Software Packages" />
+                <input type="checkbox" id="issue_software_packages" name="issues[]" value="Software Packages" />
 
                 <label htmlFor="issue_software_packages">Software Packages</label>
             </div>
             
             <div className={styles.checkbox_with_label_container}>
-                <input type="checkbox" id="issue_others" name="issues" value="Others" />
+                <input type="checkbox" id="issue_others" name="issues[]" value="Others" />
 
                 <label htmlFor="issue_others">Others</label>
             </div>
