113a23aa7e3fb5c55ffe31b664f8812473130e95
YaminNather <2001s.yn@gmail.com>
2022-12-04 11:22:19 +0530

    Added images to testimonials

--- components/LandingPage/testimonial_section/testimonials.tsx
+++ components/LandingPage/testimonial_section/testimonials.tsx
@@ -1,5 +1,7 @@
 import { TestimonialCardProps } from "./testimonial_card";
 import priyadarsiniBImage from "../../../public/landing-page/testimonial-section/priyadarsini-b.jpg";
+import sathishSImage from "../../../public/landing-page/testimonial-section/sathish-s.jpg";
+import venkatJImage from "../../../public/landing-page/testimonial-section/venkat-j.jpg";
 
 export const testimonials: TestimonialCardProps[] = [
     {
@@ -8,13 +10,13 @@ export const testimonials: TestimonialCardProps[] = [
         testimonial: "Trained laptop service experts in Coimbatore, excellent service and trustworthy. I got my laptop fixed and delivered in less than a day!",
     },
     {
-        name: "Satish S",
-        image: priyadarsiniBImage,
+        name: "Sathish S",
+        image: sathishSImage,
         testimonial: "Excellent customer care. Best laptop service in Pollachi with genuine parts. They also service CPUs and upgrade CPUs.",
     },
     {
         name: "Venkat J",
-        image: priyadarsiniBImage,
+        image: venkatJImage,
         testimonial: "We give our office laptops to repair for the past 3 years to IT Konnect Coimbatore and they have the best customer support!",
     },
 ];
