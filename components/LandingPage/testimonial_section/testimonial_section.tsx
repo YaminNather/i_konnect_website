7680cb50c2db3e59746b40797d7bde447781f8d0
YaminNather <2001s.yn@gmail.com>
2022-12-04 11:50:14 +0530

    Added background images in testimonial sections and selling points section

--- components/LandingPage/testimonial_section/testimonial_section.tsx
+++ components/LandingPage/testimonial_section/testimonial_section.tsx
@@ -1,15 +1,26 @@
 import { FC } from "react";
+import Image from "next/future/image";
 import { testimonials } from "./testimonials";
 import { TestimonialCard } from "./testimonial_card";
 import styles from "./testimonial_section_styles.module.scss";
 
+import backgroundImage from "../../../public/landing-page/testimonial-section/background.webp";
+
 export const TestimonialSection: FC = (props) => {
     return (
         <section id="testimonial_section" className={styles.testimonial_section}>
-            <h1>What our customers say about us...</h1>
+            <div className={styles.background}>
+                <Image src={backgroundImage} />
 
-            <div className={styles.testimonials_grid}>
-                {testimonials.map( (value, index, array) => <TestimonialCard key={index} {...value} /> )}
+                <div className={styles.background_tint} />
+            </div>
+
+            <div className={styles.main_content}>
+                <h1>What our customers say about us...</h1>
+
+                <div className={styles.testimonials_grid}>
+                    {testimonials.map( (value, index, array) => <TestimonialCard key={index} {...value} /> )}
+                </div>
             </div>
         </section>
     );
--- components/sales_lead_page/selling_points_section/selling_points_section.tsx
+++ components/sales_lead_page/selling_points_section/selling_points_section.tsx
@@ -1,22 +1,29 @@
 import classNames from "classnames";
 import { FC } from "react";
+import Image from "next/future/image";
 
 import styles from "./selling_points_section_styles.module.scss";
+
+import backgroundImage from "../../../public/sales-lead-page/selling-points-section/background.webp";
 
 export const SellingPointsSection: FC = (props) => {
     return (
         <section id="selling_points_section" className={styles.selling_points_section}>
-            <div className={classNames("container", styles.container)}>
-                <div className={styles.grid_cell}>
-                    <strong>Diagonised in less than 4 hours</strong>
+            <Image src={backgroundImage} className={styles.background_image} />
 
-                    <p>IT Konnect offers top of the line services and spares some of the most well-known brands across Coimbatore</p>
-                </div>
-                
-                <div className={styles.grid_cell}>
-                    <strong>Very accessible</strong>
+            <div className={styles.main_content}>
+                <div className={classNames("container", styles.container)}>
+                    <div className={styles.grid_cell}>
+                        <strong>Diagonised in less than 4 hours</strong>
 
-                    <p>We are spread accross 8 locations in Coimbatore and we are open seven days in a week</p>
+                        <p>IT Konnect offers top of the line services and spares some of the most well-known brands across Coimbatore</p>
+                    </div>
+                    
+                    <div className={styles.grid_cell}>
+                        <strong>Very accessible</strong>
+
+                        <p>We are spread accross 8 locations in Coimbatore and we are open seven days in a week</p>
+                    </div>
                 </div>
             </div>
         </section>
